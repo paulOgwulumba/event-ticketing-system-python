f83703d6225890c7c2c676c6f59cbf9940fcaae6
Paul Ogwulumba <68940073+paulOgwulumba@users.noreply.github.com>
2024-05-19 10:29:51 +0100

    Create smart contract

--- projects/event-ticketing-system/smart_contracts/event_ticketing/contract.py
+++ projects/event-ticketing-system/smart_contracts/event_ticketing/contract.py
@@ -1,7 +1,97 @@
-from algopy import ARC4Contract, arc4
+from algopy import *
 
 
 class EventTicketing(ARC4Contract):
-    @arc4.abimethod()
-    def hello(self, name: arc4.String) -> arc4.String:
-        return "Hello, " + name
+    asset_id: UInt64
+    ticket_price: UInt64
+    asset_created: bool
+    num_of_tickets: UInt64
+
+    # create application
+    @arc4.abimethod(allow_actions=['NoOp'], create='require')
+    def create_application(self, num_of_tickets: UInt64, ticket_price: UInt64) -> None:
+        assert num_of_tickets > 0, 'Number of tickets must be greater than 0'
+        self.ticket_price = ticket_price
+        self.asset_created = True
+        self.num_of_tickets = num_of_tickets
+    
+    # bootstrap ticket asset
+    @arc4.abimethod
+    def bootstrap_ticket_asset(self, mbr_pay: gtxn.PaymentTransaction) -> UInt64:
+        assert Txn.sender == Global.creator_address
+        assert self.asset_created == False
+
+        assert mbr_pay.receiver == Global.current_application_address
+        assert mbr_pay.amount >= Global.min_balance + Global.asset_create_min_balance
+
+        asset_id = (
+            itxn.AssetConfig(
+                decimals=0,
+                total=self.num_of_tickets,
+                default_frozen=False,
+                freeze=Global.current_application_address,
+            )
+            .submit()
+            .created_asset.id
+        )
+
+        self.asset_id = asset_id
+
+        log(asset_id)
+
+        return self.asset_id
+
+    # change ticket price
+    @arc4.abimethod
+    def update_price(self, new_price: UInt64) -> None:
+        assert Txn.sender == Global.creator_address
+        self.ticket_price = new_price
+
+    # View current price
+    @arc4.abimethod
+    def view_price(self) -> UInt64:
+        return self.ticket_price
+    
+    # View asset id
+    @arc4.abimethod
+    def view_asset(self) -> UInt64:
+        return self.asset_id
+
+    # buy ticket
+    @arc4.abimethod
+    def buy(self, buyer_txn: gtxn.PaymentTransaction) -> None:
+        assert buyer_txn.receiver == Global.current_application_address
+        assert buyer_txn.amount == self.ticket_price, 'Wrong price provided'
+        assert self.num_of_tickets > 0, 'No tickets left'
+
+        ticket_asset = Asset(self.asset_id)
+        assert Txn.sender.is_opted_in(ticket_asset), 'Asset not opted in'
+        assert ticket_asset.balance(buyer_txn.sender) == 0, 'Ticket already bought'
+
+        itxn.AssetTransfer(
+            xfer_asset=self.asset_id,
+            asset_receiver=buyer_txn.sender,
+            asset_amount=1,
+            fee=0,
+        ).submit()
+
+        self.num_of_tickets = self.num_of_tickets - 1
+
+
+    # delete the application
+    @arc4.abimethod(allow_actions=['DeleteApplication'])
+    def deleteApplication(self) -> None:
+        assert Txn.sender == Global.creator_address
+
+        itxn.AssetTransfer(
+            xfer_asset=self.asset_id,
+            asset_receiver=Global.creator_address,
+            asset_amount=0,
+            asset_close_to=Global.creator_address,
+        ).submit()
+
+        itxn.Payment(
+            amount=0,
+            close_remainder_to=Global.creator_address,
+            receiver=Global.creator_address,
+        ).submit()
